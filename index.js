d3d130dcb8d82feadd639f7e0f3cfb3165d0da01
WorkPC <luisvruge@hotmail.com>
2019-08-05 13:42:36 -0500

    Socket server is ready.

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ http = require('http');
 app = express();
 server = http.createServer(app);
 io = require('socket.io').listen(server);
+const CronJob = require('cron').CronJob;
 
-const MIN_PLAYERS = 1;
+const MIN_PLAYERS = 2;
 let TOTAL_PEOPLE_IN_THE_ROOM = 0;
-const IS_BINGO = false;
-const ROOM = "SALA 1";
-const INTERVAL_BINGO_NUMBER = 8 * 1000;
 
-const POOL_NUMBERS = [];
+const ROOM = "SALA 1";
+const INTERVAL_BINGO_NUMBER = 12 * 1000;
+
+let POOL_NUMBERS = [];
 
 const PATTERN = ["HORIZONTAL", "VERTICAL", "DIAGONAL", "CORNERS"];
 
-const BINGOS_TO_PLAY = 1; 
+let BINGOS_TO_PLAY = 1; 
+
+var job;
 
 
 
@@ -25,28 +28,71 @@ app.get('/', (req, res) =>{
 
 io.on('connection', socket =>{
     console.log('New user is in the room.')
+
     //Increment the counter of the users.
     TOTAL_PEOPLE_IN_THE_ROOM++;
 
     //Add user to the room.
     socket.join(ROOM);
-
     if(TOTAL_PEOPLE_IN_THE_ROOM >= MIN_PLAYERS){
         sendBingoNumber(socket);
 
-        sendSettingsGame(socket);
     }else{
-        console.log('Waiting to other players... Online players ATM: '+COUNT_PLAYERS);
+        console.log('Waiting to other players... Online players ATM: '+TOTAL_PEOPLE_IN_THE_ROOM);
     }
+
+
+    //Someone, win!
+    socket.on('BINGO', () =>{
+
+        // Need to send the pool number ?
+        //Notify all user except the bingo winner.
+        BINGOS_TO_PLAY --;
+
+        if(BINGOS_TO_PLAY == 0){
+            console.log('BINGO!');
+            socket.broadcast.emit('gameOver', "");
+            restartInitRoomConfig();
+        }
+        
+        
+    });
+
+    socket.on('disconnect', function() {
+        TOTAL_PEOPLE_IN_THE_ROOM --;
+        if(TOTAL_PEOPLE_IN_THE_ROOM == 0){
+            restartInitRoomConfig();
+        }
+        console.log('Someone left the room.')
+    
+    });
     
 
 });
 
 
+
+
+
+
+// ************** FUNCTIONS ***************//
+
+
+let restartInitRoomConfig =() =>{
+    job.stop(); 
+    POOL_NUMBERS = [];
+    TOTAL_PEOPLE_IN_THE_ROOM = 0;
+    BINGOS_TO_PLAY = 1;
+}
+
 let sendBingoNumber = (socket) => {
-    setInterval(function(){ 
-        generateBingoNumber(socket)
-    } , INTERVAL_BINGO_NUMBER);
+
+    //Task every 8 seconds
+    job = new CronJob('*/8 * * * * *', function() {
+        generateBingoNumber(socket);
+    });
+
+    job.start();
 }
 
 let generateBingoNumber =(socket) =>{
@@ -56,8 +102,6 @@ let generateBingoNumber =(socket) =>{
         number = Math.floor(Math.random() * 75) + 1;
         if(!POOL_NUMBERS.includes(number))
             flag = true;
-        else
-            console.log('Number repeated: '+number)
     }while (!flag);
     
     //Broadcast the number generated.
@@ -80,10 +124,7 @@ let generateBingoNumber =(socket) =>{
     }
 }
 
-let sendSettingsGame = () =>{
-    
 
-}
 
 
 
